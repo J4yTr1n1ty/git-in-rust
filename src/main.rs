cfe9b433375a82c20dc0d9379413a2a2e0cd842c
Jay <julian.graf@projectchaos.ch>
2024-05-02 11:00:35 +0200

    feat: added hash-file command

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,15 @@ use std::env;
 #[allow(unused_imports)]
 use std::fs;
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::path::Path;
 
 use clap::Parser;
 use clap::Subcommand;
 use flate2::read::ZlibDecoder;
+use flate2::write::ZlibEncoder;
+use flate2::Compression;
+use sha1::{Digest, Sha1};
 use std::io::prelude::*;
 
 #[derive(Parser, Debug)]
@@ -27,6 +31,13 @@ enum Command {
         pretty_print: bool,
 
         object_hash: String,
+    },
+    #[clap(about = "Hashes a given File")]
+    HashFile {
+        #[clap(short = 'w')]
+        write: bool,
+
+        file_path: String,
     },
 }
 
@@ -50,6 +61,13 @@ fn main() {
             }
 
             cat_file_pretty_print(&object_hash)
+        }
+        Command::HashFile { write, file_path } => {
+            if !write {
+                todo!()
+            }
+
+            hash_file(&file_path);
         }
     }
 }
@@ -94,6 +112,53 @@ fn cat_file_pretty_print(object_hash: &str) {
     print!("{}", parts[1]);
 }
 
+fn hash_file(file_path: &str) {
+    // Get Content of File
+    let path = Path::new(file_path);
+    if !path.exists() {
+        panic!("Given file path is invalid.")
+    }
+    let mut file = File::open(&path).expect("File unable to be opened.");
+    let mut content = Vec::new();
+    file.read_to_end(&mut content).unwrap();
+    // Get sha1 hash of the file before Compression and output it
+    let mut hasher = Sha1::new();
+
+    hasher.update(&content);
+
+    let file_hash = hasher.finalize();
+    let hex_hash = hex::encode(file_hash);
+    // Compress file contents
+    let compressed_file_contents = compress(content);
+    // Create folder with first two characters of the hash
+    let root_folder;
+    match find_git_root() {
+        Some(value) => root_folder = value,
+        None => panic!("No git repository found in this or any parent directory."),
+    }
+    let object_folder_str = format!("{root_folder}/.git/objects/{}", &hex_hash[0..2]);
+    let object_folder = Path::new(&object_folder_str);
+    if !object_folder.exists() {
+        // Create directory if not exists (dir_all means also create parent directories)
+        let create_result = fs::create_dir_all(object_folder);
+        if !create_result.is_ok() {
+            panic!("Unable to create object directory.")
+        }
+    }
+    // Write compressed contents to file with name of the remaining characters of the hash
+    let object_file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(object_folder.join(&hex_hash[2..]));
+    if !object_file.is_ok() {
+        panic!("Failed to open file to write to.")
+    }
+
+    let _ = object_file.unwrap().write_all(&compressed_file_contents);
+
+    println!("{}", hex_hash);
+}
+
 fn find_git_root() -> Option<String> {
     let mut current_dir = env::current_dir().expect("Failed to get current directory");
 
@@ -123,4 +188,19 @@ fn decompress(content: Vec<u8>) -> String {
     let mut s = String::new();
     d.read_to_string(&mut s).unwrap();
     return s;
+}
+
+fn compress(content: Vec<u8>) -> Vec<u8> {
+    let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+
+    // Convert the size of the content to a string
+    let size_str = content.len().to_string();
+
+    // Create a new vector that starts with "blob ", followed by the size, a null byte, and the content
+    let mut to_compress = format!("blob {}\0", size_str).into_bytes();
+    to_compress.extend_from_slice(&content);
+
+    e.write_all(&to_compress).unwrap();
+
+    return e.finish().unwrap();
 }
